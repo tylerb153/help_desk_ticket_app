bacc275670178f79c90486d9f0259a286dc9cd4d
Tyler Bischoff <tbischoff2@ymail.com>
2023-04-06 04:29:00 -0400

    Buttons work and are sized with a stylesheet

--- src/main/java/bischoff/tyler/PrimaryController.java
+++ src/main/java/bischoff/tyler/PrimaryController.java
@@ -7,6 +7,22 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("primary");
+        System.out.println("Refreshed");
+    }
+    
+    @FXML
+    private void filter() {
+        System.out.println("Filter Button Pushed");
+    }
+
+    @FXML
+    private void addModify() {
+        System.out.println("Add/Modify Button Pushed");
+    }
+
+    @FXML
+    private void delete() {
+        System.out.println("Delete Button Pushed");
     }
 }
