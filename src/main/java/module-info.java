5a8309c46efda5585b4392f44135e2716aed101a
Tyler Bischoff <tbischoff2@ymail.com>
2023-04-06 02:53:27 -0400

    Set up and fixed transitive error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module bischoff.tyler {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens bischoff.tyler to javafx.fxml;
     exports bischoff.tyler;
