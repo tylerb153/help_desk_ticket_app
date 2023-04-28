1eff9089b32d23ea7d5c3c240aaeea2cd417a090
Tyler Bischoff <tbischoff2@ymail.com>
2023-04-28 02:56:32 -0400

    SQL Connected

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module bischoff.tyler {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
     requires transitive javafx.graphics;
 
     opens bischoff.tyler to javafx.fxml;
